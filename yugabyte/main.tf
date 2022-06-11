dc1869a6abbfd9e780584a33a3b7f249ecbd48f1
Tom <info@tomhoward.codes>
2022-06-11 10:55:34 -0400

    fix(db): add gateway and route table

--- yugabyte/main.tf
+++ yugabyte/main.tf
@@ -4,28 +4,6 @@ resource "aws_key_pair" "terraform_yugabyte" {
   public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKInn8iWpWWPIi4JXMB9So54UDIqXTSeUnaDm7FUoXlY"
 }
 
-resource "aws_vpc" "prod_main" {
-  cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_subnet" "prod_main_use1a" {
-  availability_zone = "us-east-1a"
-  cidr_block        = "10.0.1.0/24"
-  vpc_id            = aws_vpc.prod_main.id
-}
-
-resource "aws_subnet" "prod_main_use1b" {
-  availability_zone = "us-east-1b"
-  cidr_block        = "10.0.2.0/24"
-  vpc_id            = aws_vpc.prod_main.id
-}
-
-resource "aws_subnet" "prod_main_use1c" {
-  availability_zone = "us-east-1c"
-  cidr_block        = "10.0.3.0/24"
-  vpc_id            = aws_vpc.prod_main.id
-}
-
 resource "local_file" "private_key" {
   filename        = "${path.module}/yugabyte.key"
   content_base64  = var.ssh_key
---
+++ yugabyte/vpc.tf
@@ -0,0 +1,41 @@
+
+
+resource "aws_vpc" "prod_main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_internet_gateway" "prod_main" {
+  vpc_id = aws_vpc.prod_main.id
+}
+
+resource "aws_route_table" "prod_main_external" {
+  vpc_id = aws_vpc.prod_main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.prod_mian.id
+  }
+}
+
+resource "aws_main_route_table_association" "prod_main" {
+  vpc_id         = aws_vpc.prod_main.id
+  route_table_id = aws_route_Table.prod_main_external.id
+}
+
+resource "aws_subnet" "prod_main_use1a" {
+  availability_zone = "us-east-1a"
+  cidr_block        = "10.0.1.0/24"
+  vpc_id            = aws_vpc.prod_main.id
+}
+
+resource "aws_subnet" "prod_main_use1b" {
+  availability_zone = "us-east-1b"
+  cidr_block        = "10.0.2.0/24"
+  vpc_id            = aws_vpc.prod_main.id
+}
+
+resource "aws_subnet" "prod_main_use1c" {
+  availability_zone = "us-east-1c"
+  cidr_block        = "10.0.3.0/24"
+  vpc_id            = aws_vpc.prod_main.id
+}
+
