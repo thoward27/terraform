86dd2557e43e57f48e4b02955da46fd7fa7edc36
Tom <info@tomhoward.codes>
2022-07-04 12:28:31 -0400

    chore(iam): add dev cli to outputs

--- iam/src/outputs.tf
+++ iam/src/outputs.tf
@@ -6,3 +6,11 @@ output "aws_iam_access_key_id_terraform_cloud_prod" {
 output "aws_iam_access_key_secret_terraform_cloud_prod" {
   value = aws_iam_access_key.terraform_cloud_prod.encrypted_secret
 }
+
+output "aws_iam_access_key_id_dev_cli_access" {
+  value = aws_iam_access_key.dev_cli_access.id
+}
+
+output "aws_iam_access_key_secret_dev_cli_access" {
+  value = aws_iam_access_key.dev_cli_access.encrypted_secret
+}
