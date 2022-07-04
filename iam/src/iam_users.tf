feb89b58e4a7e56ae87dd23aa38e795c510e7cc4
Tom <info@tomhoward.codes>
2022-07-04 12:22:59 -0400

    chore: add dev cli user with admin

--- iam/src/iam_users.tf
+++ iam/src/iam_users.tf
@@ -26,3 +26,32 @@ resource "aws_iam_user_policy_attachment" "terraform_cloud_prod_admin" {
   user       = aws_iam_user.terraform_cloud_prod.name
   policy_arn = aws_iam_policy.admin.arn
 }
+
+resource "aws_iam_user" "dev_cli_access" {
+  name = "dev-cli-access"
+  path = "/dev/"
+
+  tags = {
+    managed = true
+  }
+}
+
+resource "aws_am_access_key" "dev_cli_access" {
+  user    = aws_iam_user.dev_cli_access.name
+  pgp_key = var.base64_pgp_public_key
+}
+
+resource "aws_iam_user_policy_attachment" "dev_cli_access_read" {
+  user       = aws_iam_user.dev_cli_access.name
+  policy_arn = aws_iam_policy.read.arn
+}
+
+resource "aws_iam_user_policy_attachment" "dev_cli_access_write" {
+  user       = aws_iam_user.dev_cli_access.name
+  policy_arn = aws_iam_policy.write.arn
+}
+
+resource "aws_iam_user_policy_attachment" "dev_cli_access_admin" {
+  user       = aws_iam_user.dev_cli_access.name
+  policy_arn = aws_iam_policy.admin.arn
+}
