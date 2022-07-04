4dcc2e24b68bb3309da35c05580295e20b99c39b
Tom <info@tomhoward.codes>
2022-07-04 12:33:55 -0400

    chore(iam): add dev creds to iam outputs

--- iam/outputs.tf
+++ iam/outputs.tf
@@ -7,3 +7,12 @@ output "terraform_cloud_access_key_secret" {
   value     = module.default.aws_iam_access_key_secret_terraform_cloud_prod
   sensitive = true
 }
+
+output "dev_cli_access__key_id" {
+  value = module.default.aws_iam_access_key_id_dev_cli_access
+}
+
+output "dev_cli_access_key_secret" {
+  value     = module.default.aws_iam_access_key_secret_dev_cli_access
+  sensitive = true
+}
